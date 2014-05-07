aa96a67265337b6658803f9f181351ec39bbaca5
morganloehr <morganloehr@gmail.com>
2014-05-07 15:58:00 -0700

    added sliding nav button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -106,3 +106,19 @@ jQuery(document).ready(function($) {
     w.addEventListener( "orientationchange", restoreZoom, false );
     w.addEventListener( "devicemotion", checkTilt, false );
 })( this );
+
+
+$(document).ready(function(){
+
+  $('.js-menu-trigger').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+  $('.js-menu-screen').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
